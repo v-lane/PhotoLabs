7cf5778182b6e451b29f26916896efedf9d8d723
v-lane <victoria.lane.m@gmail.com>
2024-03-20 16:02:46 -0400

    Update photos data path

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -2,23 +2,22 @@ import React from "react";
 
 import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
-import photos from "mocks/photos";
 
-const PhotoList = () => {
-const photosList = photos.map(photo => {
-  return <PhotoListItem
-  key={ photo.id }
-  city={ photo.location.city }
-  country={ photo.location.country }
-  name={ photo.user.name }
-  profilePic={ photo.user.profile }
-  photoPic={ photo.urls.full }
-  />;
-});
+const PhotoList = (props) => {
+  const photos = props.photos.map(photo => {
+    return <PhotoListItem
+      key={photo.id}
+      city={photo.location.city}
+      country={photo.location.country}
+      name={photo.user.name}
+      profilePic={photo.user.profile}
+      photoPic={photo.urls.full}
+    />;
+  });
 
   return (
     <ul className="photo-list">
-      {photosList}
+      {photos}
     </ul>
   );
 };
