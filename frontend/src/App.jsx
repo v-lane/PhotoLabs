dbe092b583780a97f6a95296092ce987b7902a1a
v-lane <victoria.lane.m@gmail.com>
2024-03-20 12:17:22 -0400

    Add homepage route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
 import './App.scss';
-import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
-import TopNavigation from 'components/TopNavigationBar';
+import HomeRoute from 'routes/HomeRoute';
 
 const App = () => {
   return (
     <div className="App">
-      <TopNavigation />
-      {/* <TopicList/> */}
-      <PhotoList/>
+      <HomeRoute/>
     </div>
   );
 };
