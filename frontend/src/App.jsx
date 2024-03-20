2591c881219f864fbda2cce288fcd4568d0a5d1d
v-lane <victoria.lane.m@gmail.com>
2024-03-20 11:16:03 -0400

    Add user ability to like photo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
-import PhotoFavButton from 'components/PhotoFavButton';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-// Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {/* <PhotoList/> */}
-      <PhotoFavButton />
+      <PhotoList/>
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 
 
@@ -8,16 +9,17 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   return (
     <article className="photo-list__item">
-    <img className="photo-list__image" src={props.photoPic}/>
-    <section className="photo-list__user-details">
-    <img className="photo-list__user-profile"src={props.profilePic}/>
-    <div>
-    <h2 className="photo-list__user-info">{props.name}</h2>
-    <h2 className="photo-list__user-location">{props.city}, {props.country}</h2>
-    </div>
-    </section>
+      <PhotoFavButton />
+      <img className="photo-list__image" src={props.photoPic} />
+      <section className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.profilePic} />
+        <div>
+          <h2 className="photo-list__user-info">{props.name}</h2>
+          <h2 className="photo-list__user-location">{props.city}, {props.country}</h2>
+        </div>
+      </section>
     </article>
-  )
+  );
 };
 
 export default PhotoListItem;
