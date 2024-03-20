35dbd558631982f18ebdd6e42a800b57e9e1aa69
v-lane <victoria.lane.m@gmail.com>
2024-03-20 12:13:01 -0400

    Add top nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
 import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 
 const App = () => {
   return (
     <div className="App">
-      <TopicList/>
+      <TopNavigation />
+      {/* <TopicList/> */}
       <PhotoList/>
     </div>
   );
